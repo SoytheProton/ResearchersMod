357b140c19ac44a66fbf7ee11f9a656bec7da7f5
SoytheProton <alttimelinemod@gmail.com>
2025-08-27 22:32:14 -0700

    V.0.3.3.1
    -- Bug Fixes --
    > Fixed a caused by looking at your Run History while you had Electromagnetic Instability in your deck.

--- src/main/java/researchersmod/patches/AddCardToPilePatch.java
+++ src/main/java/researchersmod/patches/AddCardToPilePatch.java
@@ -4,6 +4,8 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SuppressWarnings("unused")
 public class AddCardToPilePatch {
@@ -18,7 +20,7 @@ public class AddCardToPilePatch {
     public static class onAddCardPatch {
         @SpirePrefixPatch
         public static void Prefix(CardGroup __instance, AbstractCard __c) {
-            if(__c instanceof AddToPileInterface) ((AddToPileInterface) __c).onAddCard(__instance, __c);
+            if(__c instanceof AddToPileInterface && (AbstractDungeon.currMapNode != null && (AbstractDungeon.getCurrRoom()).phase == AbstractRoom.RoomPhase.COMBAT)) ((AddToPileInterface) __c).onAddCard(__instance, __c);
         }
     }
 }
